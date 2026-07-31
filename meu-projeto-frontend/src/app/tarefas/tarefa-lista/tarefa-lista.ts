96e7e800629646882e5c8b8829ec0a78172972be
Edson Aguiar <edsonaguiar888@gmail.com>
2026-07-31 14:51:13 -0300

    Criando componentes lista-tarefas

---
+++ meu-projeto-frontend/src/app/tarefas/tarefa-lista/tarefa-lista.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefa-lista',
+  imports: [],
+  templateUrl: './tarefa-lista.html',
+  styleUrl: './tarefa-lista.css',
+})
+export class TarefaLista {}
