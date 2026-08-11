80ec4236af771cf6b1bc7367405c525df63cdb08
Edson Aguiar <edsonaguiar888@gmail.com>
2026-08-11 18:02:37 -0300

    Iniciando conversao de percistencia de dados de array para bando de dados mysql

--- tarefas_backend-api/src/app.module.ts
+++ tarefas_backend-api/src/app.module.ts
@@ -5,23 +5,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TarefasModule } from './tarefas/tarefas.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TarefasModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: process.env.DB_HOST || 'localhost',
-    //   port: 3306,
-    //   username: process.env.DB_USER || 'root',
-    //   password: process.env.DB_PASS || 'senha123',
-    //   database: process.env.DB_NAME || 'tarefas_db',
-    //   autoLoadEntities: true,
-    //   synchronize: true,   // NÃO usar em produção!
-    // }),
+  imports: [
+      // Conecta o NestJS ao banco MySQL usando as variáveis de ambiente do Docker
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || 'root',
+      database: process.env.DB_NAME || 'technook_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Carrega todas as entidades automaticamente
+      synchronize: true, // Em desenvolvimento, cria as tabelas automaticamente no banco!
+    }),
     TarefasModule,
-
-
   ],
   controllers: [AppController],
   providers: [AppService],
