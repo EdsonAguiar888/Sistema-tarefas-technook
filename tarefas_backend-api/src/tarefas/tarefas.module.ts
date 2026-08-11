f18d97b32259d96b6a67c946c573329c01056637
Edson Aguiar <edsonaguiar888@gmail.com>
2026-08-11 18:07:57 -0300

    Atualizado o tarefas.module.ts

--- tarefas_backend-api/src/tarefas/tarefas.module.ts
+++ tarefas_backend-api/src/tarefas/tarefas.module.ts
@@ -1,12 +1,33 @@
 
 
-
 import { Module } from '@nestjs/common';
-import { TarefasController } from './tarefas.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TarefasService } from './tarefas.service';
+import { TarefasController } from './tarefas.controller';
+import { TarefaEntity } from './entities/tarefa.entity';
 
 @Module({
+  imports: [
+    // Torna o repositório do TarefaEntity disponível para injeção de dependência neste módulo
+    TypeOrmModule.forFeature([TarefaEntity]),
+  ],
   controllers: [TarefasController],
-  providers: [TarefasService]
+  providers: [TarefasService],
 })
-export class TarefasModule { }
+export class TarefasModule {}
+  
+
+
+
+
+
+
+  // import { Module } from '@nestjs/common';
+  // import { TarefasController } from './tarefas.controller';
+  // import { TarefasService } from './tarefas.service';
+  
+  // @Module({
+  //   controllers: [TarefasController],
+  //   providers: [TarefasService]
+  // })
+  // export class TarefasModule { }
